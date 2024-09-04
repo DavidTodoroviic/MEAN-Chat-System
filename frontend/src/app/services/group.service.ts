489b688c07265cb0dd1c9f157adb2bda8dc0c39d
DavidTodoroviic <89335261+DavidTodoroviic@users.noreply.github.com>
2024-09-04 17:47:41 +1000

    Updated group.service.ts

--- frontend/src/app/services/group.service.ts
+++ frontend/src/app/services/group.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Group } from '../models/group.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupService {
+  private baseUrl = 'http://localhost:3000';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getGroups(): Observable<Group[]> {
+    return this.http.get<Group[]>(`${this.baseUrl}/groups`);
+  }
+
+  createGroup(name: string): Observable<Group> {
+    return this.http.post<Group>(`${this.baseUrl}/groups`, { name });
+  }
+
+  deleteGroup(groupId: string): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/groups/${groupId}`);
+  }
 }
