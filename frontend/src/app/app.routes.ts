75dfad57ed5464d5b327118f4840406847c88bec
DavidTodoroviic <89335261+DavidTodoroviic@users.noreply.github.com>
2024-09-04 17:48:32 +1000

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
+import { GroupManagementComponent } from './group-management/group-management.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'logout', component: LogoutComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'groups', component: GroupManagementComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
